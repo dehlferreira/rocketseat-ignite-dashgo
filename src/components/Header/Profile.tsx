33b01f0c112aeb91cdc89c1609f8b0bc807b8076
André Luiz Ferreira <deh.lferreira@gmail.com>
2021-10-01 16:28:03 -0300

    feat(task-17): header responsivo #17

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
+/* eslint-disable react/require-default-props */
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>André Luiz Ferreira</Text>
-        <Text color="gray.300" fontSize="small">
-          me@andrelf.dev
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>André Luiz Ferreira</Text>
+          <Text color="gray.300" fontSize="small">
+            me@andrelf.dev
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="André Luiz Ferreira"
